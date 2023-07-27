50b4ed28f7bb66cccf69e32e81e63fde04481442
Harsh Jain <harshjain0461@gmail.com>
2023-07-27 21:22:01 +0530

    Update Dropdown.js

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -10,7 +10,7 @@ const Dropdown = (props) => {
     }
     return (
         <div className='w-screen'>
-            <Link href="#" className='w-11/12 md:w-64 flex flex-row justify-between' onClick={handlClick}><p className='m-2 mb-2 w-full p-2 text-lg'>{opt}</p> <p>{drop ? <i class="drop_icon fa-solid fa-caret-up m-2 mt-6 w-full"></i> : <i class="drop_icon fa-solid fa-caret-down m-2 mt-6 w-full"></i>}</p></Link>
+            <button className='w-11/12 md:w-64 flex flex-row justify-between' style={{ textAlign: 'left' }}  onClick={handlClick}><p className='m-2 mb-2 w-full p-2 text-lg'>{opt}</p> <p>{drop ? <i class="drop_icon fa-solid fa-caret-up m-2 mt-6 w-full"></i> : <i class="drop_icon fa-solid fa-caret-down m-2 mt-6 w-full"></i>}</p></button>
             {drop && <ul>
 
                 <li className='m-2 mb-2  w-11/12 p-2 text-lg'><button onClick={() => {handlClick(),fun()}}>{op1}</button></li>
